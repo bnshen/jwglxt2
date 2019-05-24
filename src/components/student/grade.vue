3d0cd9a613f075010b244dbbf3b042583e75b60c
sunhz0117 <sunhz0117@126.com>
2019-05-24 10:37:53 +0800

    sss

--- src/components/student/grade.vue
+++ src/components/student/grade.vue
@@ -5,22 +5,31 @@
         <display :display_data = "grade"/>
     </div>
     
+  
+    <div id="sgradelist">
+        <visualizegrade v-if="gradelist.length > 0" :display_data="gradelist" 
+        :labels="labels"/>
+    
+    </div>
   </div>
 </template>
 
 <script>
 import display from "@/components/display.vue";
-//import visualize from "@/components/visualize.vue"
+import visualizegrade from "@/components/visualizegrade.vue"
 // import { constants } from 'crypto';
 // import { toASCII } from 'punycode';
 export default {
     components:{
         display,
+        visualizegrade
         
     },
     data(){
         return{
-            grade: {}
+            grade: {},
+            gradelist : [],
+            labels : [],
         }
     },
     methods:{
@@ -35,6 +44,16 @@ export default {
                 if(res.data.status == 200) {
                     console.log("get_grade_success");
                     that.grade = res.data.data.scores;
+                    console.log(that.grade);
+                    let _labels = [];
+                    let _gradelist = [];
+                    for(i in this.grade)
+                    {
+                        _labels.push(this.grade[i][1]);
+                        _gradelist.push(this.grade[i][2]);
+                    }
+                    that.labels = _labels;
+                    that.gradelist = _gradelist;
                     console.log(res.data.data.scores)
                 }
                 else {
---
+++ src/components/visualizegrade.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="visualizegrade">
+    <div id="main" style="width: 60vw;height:60vh;"></div>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+export default {
+  name: "visualizegrade",
+  props: ["display_data", "labels","title"],
+  methods: {},
+  mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+
+    // 指定图表的配置项和数据
+    var option = {
+      title:{
+        text:'成绩柱状图'
+      },
+      xAxis: {
+        type: "category",
+        data: this.labels,
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          data: this.display_data,
+          type: "bar"
+        }
+      ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+  }
+};
+</script>
+
+<style>
+#main{
+  margin: 0 auto;
+  padding: 0 auto;
+}
+</style>
